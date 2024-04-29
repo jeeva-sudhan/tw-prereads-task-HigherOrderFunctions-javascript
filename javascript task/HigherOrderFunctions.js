bffe4f91b3fd5fab22f0fccf2a2e931309b008e2
jeeva-sudhan <68820035+jeeva-sudhan@users.noreply.github.com>
2024-04-30 04:19:30 +0530

    Custom implementation of Higher Order Functions
    Map, Filter, reduce and forEach function

---
+++ javascript task/HigherOrderFunctions.js
@@ -0,0 +1,71 @@
+//let arr = [1,2,3,4,5];
+
+class Array {
+    
+    constructor() {
+        this.arr = [];
+    }
+
+    add(element) {
+       this.arr.push(element);
+    }
+
+    size() {
+        return this.arr.length;
+    }
+
+    map(callbackFunction) {
+        let length = this.arr.length;
+        let resultArr = [];
+        for(let iterator=0;iterator<length;iterator++) {
+            resultArr.push(callbackFunction(this.arr[iterator]));
+        }
+        return resultArr;
+    }
+
+    filter(callbackFunction) {
+        let length = this.arr.length;
+        let resultArr = [];
+        for(let iterator=0;iterator<length;iterator++) {
+            if(callbackFunction(this.arr[iterator])) {
+                resultArr.push(this.arr[iterator]);
+            }
+        }
+        return resultArr;
+    }
+
+    reduce(callbackFunction,initialValue) {
+        let length = this.arr.length;
+        let accumulator=initialValue, current;
+        for(let iterator=0;iterator<length;iterator++) {
+            current = this.arr[iterator];
+            accumulator = callbackFunction(accumulator,current);
+        }
+        return accumulator;
+    }
+
+    forEach(callbackFunction) {
+        let length = this.arr.length;
+        for(let iterator=0;iterator<length;iterator++) {
+            callbackFunction(this.arr[iterator]);
+        }
+    }
+}
+
+let array = new Array();
+array.add(1);
+array.add(2);
+array.add(3);
+console.log(array.size());
+
+let squareOfArr = array.map((element) => element*element);
+console.log(array.arr);
+console.log(squareOfArr);
+
+let evenArr = array.filter((element) => element%2==0);
+console.log(evenArr);
+
+let totalSumOfSquaredArr = squareOfArr.reduce((acc,curr) => acc+curr,0);
+console.log(totalSumOfSquaredArr);
+
+array.forEach((element) => console.log(element));
